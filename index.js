ae23fd4622cc664e38a05b66560d358cb6d69d57
German Kanev <kanev.gerka@gmail.com>
2024-03-09 19:48:56 +0300

    url

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 80
+const port = process.env.PORT || 4000
 const cors = require('cors');
 const path = require('path');
 app.use(cors());
