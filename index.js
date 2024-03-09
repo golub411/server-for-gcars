abf0c37469c4ef5934bd1b4b31a982f4469e1653
German Kanev <kanev.gerka@gmail.com>
2024-03-09 14:36:28 +0300

    add metamorphosese

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = process.env.PORT || 80
 const cors = require('cors');
 const path = require('path');
 app.use(cors());
@@ -8,9 +8,9 @@ app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'public')));
 
 const cards = [
-  {photo: 'http://5.35.12.49:4000/images/gls.jpg', year: "2020",model: "class gls", mark: "mersedes-benz", id: "1", color: "white", description: "Надёжный внедорожник", price:"19 000 000 руб"},
-  {photo: 'http://5.35.12.49:4000/images/cclass_white.jpg', year: "2022",model: "class c", mark: "mersedes-benz", id: "2", color: "white", description: "Элегантный седан", price:"10 000 000 руб"},
-  {photo: 'http://5.35.12.49:4000/images/sclass_black.jpg', year: "2021",model: "class s", mark: "mersedes-benz", id: "3", color: "black", description: "Седан премиум класса", price:"17 000 000 руб"},
+  {photo: '/images/gls.jpg', year: "2020",model: "class gls", mark: "mersedes-benz", id: "1", color: "white", description: "Надёжный внедорожник", price:"19 000 000 руб"},
+  {photo: '/images/cclass_white.jpg', year: "2022",model: "class c", mark: "mersedes-benz", id: "2", color: "white", description: "Элегантный седан", price:"10 000 000 руб"},
+  {photo: '/images/sclass_black.jpg', year: "2021",model: "class s", mark: "mersedes-benz", id: "3", color: "black", description: "Седан премиум класса", price:"17 000 000 руб"},
 ]
 
 app.get('/', (req, res) => {
